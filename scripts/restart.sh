cc0e728c0edd4eb705dfdc4d0f67f8aa22dce1e7
Julia Evans <julia@jvns.ca>
2020-12-07 20:57:10 -0500

    wip

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,1 +1,2 @@
-/usr/share/rvm/rubies/ruby-2.7.2/bin/bundle
+PATH=/usr/share/rvm/gems/ruby-2.7.2/bin:/usr/share/rvm/gems/ruby-2.7.2@global/bin:/usr/share/rvm/rubies/ruby-2.7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/share/rvm/bin
+bundle
