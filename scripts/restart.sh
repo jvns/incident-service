e974e16dca8b0581f8245d491c5cf7e8fe7d2fd3
Julia Evans <julia@jvns.ca>
2020-12-08 16:59:07 -0500

    wip

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,3 +1,4 @@
+export RAILS_ENV=production
 PATH=/usr/share/rvm/gems/ruby-2.7.2/bin:/usr/share/rvm/gems/ruby-2.7.2@global/bin:/usr/share/rvm/rubies/ruby-2.7.2/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/usr/share/rvm/bin
 bundle
 bundle exec rails db:migrate
