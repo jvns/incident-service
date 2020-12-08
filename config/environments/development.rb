b5336618199fa0130223f486ea087e6ec3fe7b38
Julia Evans <julia@jvns.ca>
2020-12-08 13:38:09 -0500

    wip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,7 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.hosts << /.*.ngrok.io/
 
   # Show full error reports.
   config.consider_all_requests_local = true
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'homepage/index'
   root 'homepage#index'
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
   resources :virtual_machine, only: [:index]
 end
