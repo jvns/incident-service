5fb3356234536fea8ee55f399037fd17ef4be2a5
Julia Evans <julia@jvns.ca>
2020-12-09 17:23:54 -0500

    add things

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :puzzles do
+    get 'run', on: :member
+  end
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }, skip: [:registrations]
 
   get 'homepage/index'
