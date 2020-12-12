751462b72ad913612154c6bddac13d23f428d3b3
Julia Evans <julia@jvns.ca>
2020-12-11 21:46:37 -0500

    try to make tailwind work

---
+++ scripts/watch.sh
@@ -0,0 +1,1 @@
+find  app/assets/stylesheets | entr npx tailwindcss-cli@latest build app/assets/stylesheets/tailwind.css -o public/tailwind.css
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  purge: [
+    './app/**/*.html.erb',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+}
