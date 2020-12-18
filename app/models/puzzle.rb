63379bafea901202461638ee8ca2efc3ff982291
Julia Evans <julia@jvns.ca>
2020-12-17 20:01:03 -0500

    wip

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -2,5 +2,9 @@ class Puzzle < ApplicationRecord
   def to_param
     "#{id}-#{title.parameterize}"
   end
+
+  def finished?(user)
+    puzzle_statuses.where(user_id: user.id).first&.finished || false
+  end
   has_many :puzzle_statuses, dependent: :destroy 
 end
