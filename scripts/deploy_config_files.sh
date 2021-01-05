c09160d122661394db72c987ba3a6677406d49f6
Julia Evans <julia@jvns.ca>
2021-01-05 14:52:52 -0500

    switch to envfile

--- scripts/deploy_config_files.sh
+++ scripts/deploy_config_files.sh
@@ -3,5 +3,5 @@
 #scp scripts/post-receive root@rails-box:/home/wizard-debugging-school/hooks/
 #scp wizard.key root@rails-box:/home/wizard-debugging-school-deployed/
 scp scripts/post-receive root@dockerbox:/git/incident-service.git/hooks/post-receive
-scp secrets_prod.sh root@dockerbox:/app/secrets.sh
+scp secrets_prod.sh root@dockerbox:/app/
 scp scripts/master.key root@dockerbox:/app/config/master.key
