fc2dedee005adb7b0cce56468c7e62301bcbc3e5
Julia Evans <julia@jvns.ca>
2021-01-05 09:08:27 -0500

    wip

--- scripts/deploy_config_files.sh
+++ scripts/deploy_config_files.sh
@@ -1,4 +1,5 @@
-scp scripts/nginx_rails root@rails-box:/etc/nginx/sites-enabled/rails
-scp scripts/post-receive root@rails-box:/home/wizard-debugging-school/hooks/
-scp wizard.key root@rails-box:/home/wizard-debugging-school-deployed/
-
+#!/bin/bash
+#scp scripts/nginx_rails root@rails-box:/etc/nginx/sites-enabled/rails
+#scp scripts/post-receive root@rails-box:/home/wizard-debugging-school/hooks/
+#scp wizard.key root@rails-box:/home/wizard-debugging-school-deployed/
+scp scripts/post-receive root@dockerbox:/git/incident-service.git/hooks/post-receive
