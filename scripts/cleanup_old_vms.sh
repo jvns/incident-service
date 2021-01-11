b1193dacfb2c4e6fa425833366aa1be43d5dfdfe
Julia Evans <julia@jvns.ca>
2021-01-11 10:06:17 -0500

    fix command line

--- scripts/cleanup_old_vms.sh
+++ scripts/cleanup_old_vms.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 cd /app
-docker-compose -f docker-compose-prod.yml exec rails rails cleanup_old_vms.rb
+docker-compose -f docker-compose-prod.yml exec rails rails runner cleanup_old_vms.rb
 
