89aec3b6047b71d6688e027170b28789dc7ade9e
Julia Evans <julia@jvns.ca>
2021-01-11 19:33:26 -0500

    fix cleanup script again

--- scripts/cleanup_old_vms.sh
+++ scripts/cleanup_old_vms.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-# docker-compose is in /usr/local/bin which isn't in the path that cron has for some reason
-export PATH=$PATH:/usr/local/bin
 cd /app
-docker-compose -f docker-compose-prod.yml exec rails rails runner cleanup_old_vms.rb
+/usr/local/bin/docker-compose -f docker-compose-prod.yml exec rails rails runner cleanup_old_vms.rb
 
