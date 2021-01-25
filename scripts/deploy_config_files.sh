7bb40b2ff1fb92683c8f8f29bb6f818895b37962
Julia Evans <julia@jvns.ca>
2021-01-25 08:21:03 -0500

    various things to try to fix prod

--- scripts/deploy_config_files.sh
+++ scripts/deploy_config_files.sh
@@ -2,3 +2,4 @@
 scp scripts/post-receive root@dockerbox:/git/incident-service.git/hooks/post-receive
 scp secrets_prod.sh wizard.key wizard.key.pub root@dockerbox:/app/
 scp scripts/master.key root@dockerbox:/app/config/master.key
+scp scripts/ignite.service root@dockerbox:/etc/systemd/system/ignite-manager.service
