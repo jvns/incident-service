cfb804614833a5aff89c7784eb33e2cd104e0b0c
Julia Evans <julia@jvns.ca>
2021-02-01 13:20:05 -0500

    don't log datetime twice

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
+    logger.datetime_format = ''
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
